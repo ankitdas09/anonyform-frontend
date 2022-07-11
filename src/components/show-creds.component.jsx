67daccf2c92a6c1abe6efa945fecb264c01e496d
Ankit <ankit@Ankits-MacBook-Air.local>
2022-07-11 23:33:11 +0530

    links page update

--- src/components/show-creds.component.jsx
+++ src/components/show-creds.component.jsx
@@ -7,8 +7,13 @@ const ShowCreds = ({ creds }) => {
                 creds ?
                     <div>
                         <h2>Please copy both links</h2>
-                        <h6>Form Link : https://anonyform.netlify.app/{`forms/${creds[1]}`}</h6>
-                        <h6>Responses : https://anonyform.netlify.app/{`forms/responses/${creds[1]}/${creds[0]}`}</h6>
+                        {creds[1] ?
+                            <>
+                                <h6>Form Link : https://anonyform.netlify.app/{`forms/${creds[1]}`}</h6>
+                                <h6>Responses : https://anonyform.netlify.app/{`forms/responses/${creds[1]}/${creds[0]}`}</h6>
+                            </>
+                            : 'Loading link ... Please wait'
+                        }
                         <p>Token : {creds[0] ? creds[0] : 'loading...'}</p>
                         <p>Form Id : {creds[1] ? creds[1] : 'loading...'}</p>
                         {/* <h6>Format : https://anonyform.netlify.app/forms/ formID </h6> */}
