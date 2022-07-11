0100442c50d400eb4d05381339119fef5ba80ece
Ankit <ankit@Ankits-MacBook-Air.local>
2022-07-11 16:56:40 +0530

    init

--- src/components/show-creds.component.jsx
+++ src/components/show-creds.component.jsx
@@ -6,11 +6,13 @@ const ShowCreds = ({ creds }) => {
             {
                 creds ?
                     <div>
-                        <h2>Please Note the access token and form id</h2>
-                        <h3>Token : {creds[0] ? creds[0] : 'loading...'}</h3>
-                        <h3>Form Id : {creds[1] ? creds[1] : 'loading...'}</h3>
-                        <h4>Form Link : https://anonyform.netlify.app/{`forms/${creds[1]}`}</h4>
-                        <h4>Responses : https://anonyform.netlify.app/{`forms/responses/${creds[1]}/${creds[0]}`}</h4>
+                        <h2>Please copy both links</h2>
+                        <h6>Form Link : https://anonyform.netlify.app/{`forms/${creds[1]}`}</h6>
+                        <h6>Responses : https://anonyform.netlify.app/{`forms/responses/${creds[1]}/${creds[0]}`}</h6>
+                        <p>Token : {creds[0] ? creds[0] : 'loading...'}</p>
+                        <p>Form Id : {creds[1] ? creds[1] : 'loading...'}</p>
+                        {/* <h6>Format : https://anonyform.netlify.app/forms/ formID </h6> */}
+                        {/* {/* <h6>Response Format : https://anonyform.netlify.app/forms/responses/ formID / accessToken</h6> */}
                     </div> : 'Something went wrong!'
             }
         </div >
